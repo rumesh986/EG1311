3a7f18d967a21b346fd167e6687ba659c3451c74
thebirdgr <60645728+thebirdgr@users.noreply.github.com>
2020-02-10 22:13:52 +0800

    Algorithm

---
+++ trial_codes/rumesh/index.ino
@@ -0,0 +1,21 @@
+//move forward
+    //power up all motors
+
+//while moving check for wall using ultrasound distance
+
+    //if less than 50cm stop exit control
+        //stop motors
+    //else keep going
+
+    //(subject to change depending on what the slope distance returns)
+
+    //if cannot move forward i.e. being blocked
+
+        //stop
+            //stop motors
+        //raise servo track to 30degrees
+            //use rumesh servo code
+        //move forward
+            //power motors
+
+    // weight shift at top of slope if neccessary.
