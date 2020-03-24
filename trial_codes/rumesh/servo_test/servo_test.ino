e179f979a028bce19a3db5d1d5148bfbd5360097
Rumesh <rumesh986@gmail.com>
2020-03-24 16:17:15 +0800

    converted servo_test to be used with a continous servo

--- trial_codes/rumesh/servo_test/servo_test.ino
+++ trial_codes/rumesh/servo_test/servo_test.ino
@@ -1,21 +1,29 @@
 #include <Servo.h>
 
-Servo servo_test;
+Servo servo;
 
 void setup() {
   // put your setup code here, to run once:
-  servo_test.attach(9);
+  servo.attach(9);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  servo_test.write(0);
-  delay(1000);
-  servo_test.write(45);
-  delay(1000);
-  servo_test.write(135);
-  delay(1000);
-  servo_test.write(180);
-  delay(1000);
+//  servo_test.write(0);
+//  delay(1000);
+//  servo_test.write(45);
+//  delay(1000);
+//  servo_test.write(135);
+//  delay(1000);
+//  servo_test.write(180);
+//  delay(1000);
 
+  servo.writeMicroseconds(1600);
+  delay(20000);
+  servo.writeMicroseconds(1500);
+  delay(10000);
+//  servo.writeMicroseconds(1400);
+//  delay(1000);
+  servo.writeMicroseconds(900);
+  delay(10000);
 }
